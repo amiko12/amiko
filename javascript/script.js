4cb313ca305d03475b03062e2f125ba6db488c16
Amiko <amgeladze10@gmail.com>
2022-03-03 17:21:22 +0300

    add bar functional

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+var navigation = document.getElementById('links')
+var burger = document.getElementById('burger')
+
+burger.addEventListener('click', function(){
+    navigation.classList.toggle('active');
+})
